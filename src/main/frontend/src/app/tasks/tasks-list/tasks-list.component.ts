2778c6ba9e983e17e95df9320b38c4f6bca8940a
Aristótenes <alallk@msn.com>
2018-05-12 15:33:04 -0300

    Fix local git problems

---
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../task.model';
+import {TaskService} from "../task.service";
+@Component({
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.component.html',
+    styleUrls: ['./tasks-list.component.css']
+})
+export class TasksListComponent implements OnInit {
+
+    tasks: Task[] = [];
+
+    constructor(private taskService:TaskService) { }
+
+    ngOnInit() {
+        return this.taskService.getTasks().subscribe((task: any[])=>{
+            this.tasks = task
+        },(error) => console.log(error));
+    }
+
+    getDueDateLabel(task:Task){
+        return task.completed ? 'label-success' : 'label-primary';
+    }
+
+    onTaskChange(event, task){
+        // this.taskService.saveTask(task, event.target.checked).subscribe();
+        console.log('task has changed')
+    }
+
+}
